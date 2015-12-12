a54b79193cec186c77a3a7862a223d973a0fca25
Chris <ignatz@gmail.com>
2015-12-12 10:44:43 -0500

    Chris - Restructured and added McRibadamus Clojure App

--- clojure/mcribadamus/project.clj
+++ clojure/mcribadamus/project.clj
@@ -1,4 +1,4 @@
-(defproject mcribadamus "0.1.0-SNAPSHOT"
+(defproject mcribadamus "0.2.0-SNAPSHOT"
   :description "For centuries people have looked to the wisdom of Nostradamus in order to see into the future. In modern times few problems have vexed mankind more than when the McRib(tm) sandwich will be available. Now, thanks to the power of McRibadamus, they can know the fate of the value meal they love so much"
   :url "https://github.com/ColumbusCodeClub/mcribadamus"
   :license {:name "Eclipse Public License"
